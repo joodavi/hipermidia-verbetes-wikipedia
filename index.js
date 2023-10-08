0a80c62818371a8730c17eb0d22c94dd3346fd33
joodavi <joodavi@live.com>
2023-10-08 13:54:58 -0300

    feat cache

--- index.js
+++ index.js
@@ -8,106 +8,121 @@ const searchButton = document.getElementById("searchButton")
 var searchTerm = ""
 var searchList = ""
 
-var cache = {}
+var cacheResult = {}
 var pagesArray = {}
+var cacheXml = []
 
-function loadXml() {
+function loadingXml() {
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             var xmlString = this.responseText
             xmlString = parser.parseFromString(xmlString, "text/html")
             pagesArray = xmlString.querySelectorAll("page")
             console.log("tempo de carregamento do xml: ")
-            return pagesArray
+            pagesArray.forEach(function (page) {
+                var title = page.querySelector("title").textContent
+                var text = page.querySelector("text").textContent
+                cacheXml.push({
+                    title: title,
+                    text: text,
+                    occurrences: countWordOccurrences(title, text)
+                })
+                
+            })
+            return cacheXml
         }
     }
-    xhttp.open("GET", "data/verbetesWikipediaFull.xml", true)
+    xhttp.open("GET", "data/verbetesWikipedia.xml", true)
     xhttp.send()
 }
 
-loadXml()
+loadingXml()
 
-const search = () => {
+function search() {
     searchTerm = document.getElementById("searchInput").value.toLowerCase()
     searchButton.addEventListener("click", addStyle())
+    if (cacheResult[searchTerm]) {
+        results = cacheResult[searchTerm]
 
-    if (cache[searchTerm]) {
-        pagesObject = cache[searchTerm]
-
-        showResults(pagesObject)
+        showResults(results)
         console.log(`buscando no cache o termo "${searchTerm}"`)
     } else {
         if ((searchTerm === "") || (searchTerm === " ") || (searchTerm.length <= 3)) {
             searchEmpty()
         } else {
             pagesObject = []
+            cacheXml.forEach(function (page) {
 
-            pagesArray.forEach(function (page) {
-                var title = page.querySelector("title").textContent
-                var text = page.querySelector("text").textContent
-
-                if (text.toLowerCase().includes(searchTerm)) {
+                if (page.title.includes(searchTerm) || page.text.includes(searchTerm)) {
                     pagesObject.push({
-                        title: title,
-                        text: text,
-                        occurrences: countOccurrences(title, text, searchTerm)
+                        title: page.title,
+                        text: page.text,
+                        occurrences: page.result[word]
                     })
                 }
             })
-
-            pagesObject = pagesObject.sort(function (a, b) {
-                return b.occurrences - a.occurrences
-            }).slice(0, 50)
-
-            cache[searchTerm] = pagesObject
-
-            showResults(pagesObject)
-
+            
+            var results = cacheXml.filter(entry => {
+                const titleMatch = entry.title.toLowerCase().includes(searchTerm)
+                const textMatch = entry.text.toLowerCase().includes(searchTerm)
+        
+                return titleMatch || textMatch
+            })
+            
+            console.log(results.sort((a, b) => b.occurrences[searchTerm] - a.occurrences[searchTerm]))
+    
+            cacheResult[searchTerm] = results
+        
             console.log(`fazendo uma nova pesquisa com o termo "${searchTerm}"`)
+            showResults(results.slice(0, 50))
         }
     }
 }
 
-function countOccurrences(title, text, searchTerm) {
-    title = title.toLowerCase().split(' ')
-    text = text.toLowerCase().split(' ')
-    
-    var count = 0
-
-    text.forEach(function (text) {
-        if (text.includes(searchTerm)) {
-            count++
-        }
-    })
-
-    title.forEach(function (title) {
-        if (title.includes(searchTerm)) {
-            count += 10
-        }
-    })
-
-    return count
-}
-
-function showResults(pagesObject) {
-    pagesObject.forEach(function (page) {
+function showResults(results) {
+    searchList = ""
+    results.forEach(function (page) {
         searchList +=
             "<summary>" +
-            page.title + " <span>" + page.occurrences + " ocorrências" + "</span>" +
+            page.title +
             "<details>" +
             page.text +
             "</details>" +
             "</summary>"
     })
     document.getElementById("xmlData").innerHTML = searchList
-    searchList = ""
+}
+
+function countWordOccurrences(title, text) {
+    const result = {}
+
+    title = title.toLowerCase().split(' ').filter((word) => word.length >= 4)
+    text = text.toLowerCase().split(' ').filter((word) => word.length >= 4)
+
+    for (let word of title) {
+        if (result[word]) {
+            result[word] += 10
+        } else {
+            result[word] = 10
+        }
+    }
+
+    for (let word of text) {
+        if (result[word]) {
+            result[word]++
+        } else {
+            result[word]= 1
+        }
+    }
+
+    return result
 }
 
 const searchEmpty = () => {
     searchList += "<p style='padding: 0 3.5em'>" +
         "Nenhum resultado encontrado" +
         "</p>"
-    
+
     document.getElementById("xmlData").innerHTML = searchList
     searchList = ""
 }
---
+++ lastGuardian.js
@@ -0,0 +1,120 @@
+const parser = new DOMParser()
+const xhttp = new XMLHttpRequest()
+
+const hero = document.getElementById("hero")
+const searchInput = document.getElementById("searchInput")
+const searchButton = document.getElementById("searchButton")
+
+var searchTerm = ""
+var searchList = ""
+
+var cache = {}
+var pagesArray = {}
+
+function loadXml() {
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            var xmlString = this.responseText
+            xmlString = parser.parseFromString(xmlString, "text/html")
+            pagesArray = xmlString.querySelectorAll("page")
+            console.log("tempo de carregamento do xml: ")
+            return pagesArray
+        }
+    }
+    xhttp.open("GET", "data/verbetesWikipediaFull.xml", true)
+    xhttp.send()
+}
+
+loadXml()
+
+const search = () => {
+    searchTerm = document.getElementById("searchInput").value.toLowerCase()
+    searchButton.addEventListener("click", addStyle())
+
+    if (cache[searchTerm]) {
+        pagesObject = cache[searchTerm]
+
+        showResults(pagesObject)
+        console.log(`buscando no cache o termo "${searchTerm}"`)
+    } else {
+        if ((searchTerm === "") || (searchTerm === " ") || (searchTerm.length <= 3)) {
+            searchEmpty()
+        } else {
+            pagesObject = []
+
+            pagesArray.forEach(function (page) {
+                var title = page.querySelector("title").textContent
+                var text = page.querySelector("text").textContent
+
+                if (text.toLowerCase().includes(searchTerm) || title.toLowerCase().includes(searchTerm)) {
+                    pagesObject.push({
+                        title: title,
+                        text: text,
+                        occurrences: countOccurrences(title, text, searchTerm)
+                    })
+                }
+            })
+
+            pagesObject = pagesObject.sort(function (a, b) {
+                return b.occurrences - a.occurrences
+            }).slice(0, 50)
+
+            cache[searchTerm] = pagesObject
+
+            showResults(pagesObject)
+
+            console.log(`fazendo uma nova pesquisa com o termo "${searchTerm}"`)
+        }
+    }
+}
+
+function countOccurrences(title, text, searchTerm) {
+    title = title.toLowerCase().split(' ')
+    text = text.toLowerCase().split(' ')
+    
+    var count = 0
+
+    text.forEach(function (text) {
+        if (text.includes(searchTerm)) {
+            count++
+        }
+    })
+
+    title.forEach(function (title) {
+        if (title.includes(searchTerm)) {
+            count += 10
+        }
+    })
+
+    return count
+}
+
+function showResults(pagesObject) {
+    pagesObject.forEach(function (page) {
+        searchList +=
+            "<summary>" +
+            page.title + " <span>" + page.occurrences + " ocorrências" + "</span>" +
+            "<details>" +
+            page.text +
+            "</details>" +
+            "</summary>"
+    })
+    document.getElementById("xmlData").innerHTML = searchList
+    searchList = ""
+}
+
+const searchEmpty = () => {
+    searchList += "<p style='padding: 0 3.5em'>" +
+        "Nenhum resultado encontrado" +
+        "</p>"
+    
+    document.getElementById("xmlData").innerHTML = searchList
+    searchList = ""
+}
+
+const addStyle = () => {
+    searchInput.style.marginBottom = "0px"
+    searchInput.style.width = "65%"
+    hero.style.width = "95%"
+    hero.style.flexDirection = "row"
+}
